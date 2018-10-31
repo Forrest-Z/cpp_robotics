00bb2e94c288fa1a93823b8db7770bd9c057806e
szyh_pc <szyh_pc@example.com>
2018-10-31 14:24:45 +0800

    add icp slam,but not ok

---
+++ slam/iterative_closest_point/cmake-build-debug/CMakeFiles/iterative_closest_point.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/forrest/project/clion_project/cpp_robotics/slam/iterative_closest_point/main.cpp" "/home/forrest/project/clion_project/cpp_robotics/slam/iterative_closest_point/cmake-build-debug/CMakeFiles/iterative_closest_point.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../lib"
+  "/usr/include/python2.7"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ slam/iterative_closest_point/cmake-build-debug/CMakeFiles/iterative_closest_point.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/iterative_closest_point.dir/main.cpp.o"
+  "iterative_closest_point.pdb"
+  "iterative_closest_point"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/iterative_closest_point.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
